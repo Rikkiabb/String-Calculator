3bc423054b8c3877f3e39c6a461dd6f2636c95f3
rikard <rikardarnar@gmail.com>
2014-10-12 17:49:14 +0000

    test for two numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,6 +2,10 @@ package is.ru.stringcalculator;
 public class Calculator{
 	public static int add(String text){
 	if(text.equals(""))	return 0;
-	return 1;
+	else if(text.contains(",")){
+		String[] numbers = text.split(",");
+		return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+	}
+	else	return 1;
 	}
 }
