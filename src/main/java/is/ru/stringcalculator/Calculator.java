a79274237bdc25e7d1fdc20b4d02ebda88412b43
rikard <rikardarnar@gmail.com>
2014-10-13 17:03:38 +0000

    Refactoring for new delimiter

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,13 +2,13 @@ package is.ru.stringcalculator;
 public class Calculator{
 	private static final String splitString = "(,)|(\n)";
 
+	private static final String newDelimiter = "//";
+
 	public static int add(String text){
 		if(text.equals(""))	return 0;
-		else if(text.startsWith("//")){
-			String newText = text.substring(4);
-			String thisDelimiter = String.valueOf(text.charAt(2));
-			String textWithComma = newText.replaceAll(thisDelimiter, ",");
-			return sum(splitNumbers(textWithComma));
+		else if(text.startsWith(newDelimiter)){
+			String formattedString = formatString(text);
+			return sum(splitNumbers(formattedString));
 		}
 		else if(delimiter(text)){
 			return sum(splitNumbers(text));
@@ -34,5 +34,12 @@ public class Calculator{
 
 	private static boolean delimiter(String numbers){
 		return (numbers.contains(",") || numbers.contains("\n"));
+	}
+
+	private static String formatString(String numbers){
+		String numbersWithoutSign = numbers.substring(4);
+		String currentDelimiter = String.valueOf(numbers.charAt(2));
+		String textWithCommas = numbersWithoutSign.replaceAll(currentDelimiter, ",");
+		return textWithCommas;
 	}
 }
